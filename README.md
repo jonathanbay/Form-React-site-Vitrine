a08ef10566c496dcb0062f0bd661930975f80826
jonathan <bayart.jonathan@laposte.net>
2023-02-26 21:09:42 +0100

    template formulaire emailJs

---
+++ README.md
@@ -0,0 +1,6 @@
+service mail : service_yae2lhw
+
+template mail : template_ilijbc9
+
+key public : gHNPGgfyp-qNE3BRY
+key private : GzI1b_4e4xKk22Wlb_oOz
--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -1,29 +1,55 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
-export const ContactUs = () => {
+const FormTemplate = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
+    const formMess = document.querySelector('.form-message')
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs
+      .sendForm(
+        // configurer dans.env pour masquerles identifiants => appeler via process.env + nom de la varible crée.
+        process.env.REACT_APP_SERVICE_MAIL, 
+        process.env.REACT_APP_TEMPLATE, 
+        form.current, 
+        process.env.REACT_APP_ID)
       .then((result) => {
           console.log(result.text);
+          form.current.reset();
+          formMess.innerHTML = "<p class='success'>Votre message a bien été envoyé </p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+
       }, (error) => {
           console.log(error.text);
+          formMess.innerHTML = "<p class='error'>Une erreur s'est produite, veuillez réessayer </p>"
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
       });
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Nom</label>
-      <input type="text" name="name" required autoComplete='off'/>
-      <label>Email</label>
-      <input type="email" name="email" required autoComplete='off'/>
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Envoyer" required/>
-    </form>
+    <div className='form-container'>       
+        <form ref={form} onSubmit={sendEmail}>
+        <label>Nom</label>
+        <input type="text" name="name" required autoComplete='off'/>
+        <label>Email</label>
+        <input type="email" name="email" required autoComplete='off'/>
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Envoyer" required/>
+        </form>
+        <div className='form-message'>
+
+        </div>
+    </div>
   );
 };
+
+export default FormTemplate;
